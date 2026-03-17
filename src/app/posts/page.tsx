c148f2adcb2fd57541169982157adf3f022cf769
chacha86 <taejincha0619@gmail.com>
2026-03-17 11:50:39 +0900

    16-2

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,9 +2,10 @@
 
 import { useEffect, useState } from "react";
 
-interface Post {
+export interface Post {
     id: number,
-    title: string
+    title: string,
+    content: string
 }
 
 export default function Home() {
